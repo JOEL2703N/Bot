01d640ffc78429812e20d30907b1ac12be7b1614
JOEL2703N <85835754+JOEL2703N@users.noreply.github.com>
2021-08-04 10:32:16 +0700

    Update ready.js

--- handlers/ready.js
+++ handlers/ready.js
@@ -3,6 +3,6 @@ module.exports = async bot => {
     console.log(`${bot.user.username} is available now!`)
     let totalUsers = bot.guilds.cache.reduce((acc, value) => acc + value.memberCount, 0)
     var activities = [ `${bot.guilds.cache.size} servers`, `${totalUsers} users!` ], i = 0;
-    setInterval(() => bot.user.setActivity(`${default_prefix}help | ${activities[i++ % activities.length]}`, { type: "WATCHING" }),50000)
+    setInterval(() => bot.user.setActivity(`${default_prefix}help | ${activities[i++ % activities.length]}`, { type: "STREAMING" }),50000)
     
 };
